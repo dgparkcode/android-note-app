e7ecbf751aeec420bc0164a1e43b968434fed0d8
dgparkcode <dgpark.code@gmail.com>
2022-11-10 18:32:25 +0900

    feat(data): implement NoteRepository and add repository module

---
+++ app/src/main/java/com/dgparkcode/note/data/di/RepositoryModule.kt
@@ -0,0 +1,21 @@
+package com.dgparkcode.note.data.di
+
+import com.dgparkcode.note.data.local.dao.NoteEntityDao
+import com.dgparkcode.note.data.repository.NoteRepositoryImpl
+import com.dgparkcode.note.domain.repository.NoteRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideNoteRepository(noteEntityDao: NoteEntityDao):NoteRepository {
+        return NoteRepositoryImpl(noteEntityDao)
+    }
+}
