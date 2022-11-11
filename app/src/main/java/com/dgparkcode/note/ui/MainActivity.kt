5218a21cc7dea2813deabf03ed9a3e0a803424f2
dgparkcode <dgpark.code@gmail.com>
2022-11-11 16:36:40 +0900

    feat(ui): update activity package to ui

--- app/src/main/java/com/dgparkcode/note/ui/MainActivity.kt
+++ app/src/main/java/com/dgparkcode/note/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dgparkcode.note
+package com.dgparkcode.note.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dgparkcode.note.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
