e65c8c0bda9da94c3983c6a3506b09a5822b6c7b
dgparkcode <dgpark.code@gmail.com>
2022-11-11 18:26:37 +0900

    feat(ui): add navigation graph and update main activity
    * 네비게이션을 설정하여 노트 목록이 표시되도록 설정합니다.
    * Hilt에서 의존성을 주입받기 위해 AndroidEntryPoint 어노테이션을 설정합니다.
    * FragmentContainerView를 사용해 네비게이션을 적용합니다.

--- app/src/main/java/com/dgparkcode/note/ui/MainActivity.kt
+++ app/src/main/java/com/dgparkcode/note/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.dgparkcode.note.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dgparkcode.note.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
