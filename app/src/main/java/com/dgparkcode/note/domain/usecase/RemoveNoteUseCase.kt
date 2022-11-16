891cebd780bc0b9e1908d166a6c71a450b6d5337
dgparkcode <dgpark.code@gmail.com>
2022-11-16 21:54:21 +0900

    feat(domain): update remove note method parameter
    * 노트를 삭제할때 노트모델 전체를 받을 필요가 없고, 아이디만으로 처리할 수 있게 합니다.

--- app/src/main/java/com/dgparkcode/note/domain/usecase/RemoveNoteUseCase.kt
+++ app/src/main/java/com/dgparkcode/note/domain/usecase/RemoveNoteUseCase.kt
@@ -1,19 +1,18 @@
 package com.dgparkcode.note.domain.usecase
 
-import com.dgparkcode.note.domain.entity.Note
 import com.dgparkcode.note.domain.repository.NoteRepository
 import javax.inject.Inject
 
 interface RemoveNoteUseCase {
 
-    suspend operator fun invoke(note: Note)
+    suspend operator fun invoke(id: Long)
 }
 
 class RemoveNoteUseCaseImpl @Inject constructor(
     private val noteRepository: NoteRepository
 ) : RemoveNoteUseCase {
 
-    override suspend fun invoke(note: Note) {
-        return noteRepository.removeNote(note = note)
+    override suspend fun invoke(id: Long) {
+        return noteRepository.removeNote(id)
     }
 }
