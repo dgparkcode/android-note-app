785e7cd714229d28ead272d1ea71149452e0d2d1
dgparkcode <dgpark.code@gmail.com>
2022-11-16 21:55:39 +0900

    feat(data): update remove note method parameter
    * 도메인 레이어에서 변경된 노트 삭제 파라미터에 맞춰서
    DAO에서 노트삭제 파라미터를 변경하고, 레파지토리를 수정한다.

--- app/src/main/java/com/dgparkcode/note/data/local/dao/NoteEntityDao.kt
+++ app/src/main/java/com/dgparkcode/note/data/local/dao/NoteEntityDao.kt
@@ -1,7 +1,6 @@
 package com.dgparkcode.note.data.local.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -17,8 +16,8 @@ interface NoteEntityDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNoteEntity(noteEntity: NoteEntity)
 
-    @Delete
-    suspend fun deleteNoteEntity(noteEntity: NoteEntity)
+    @Query("DELETE FROM note WHERE id = :id")
+    suspend fun deleteNoteEntity(id: Long)
 
     @Query("SELECT id, title, content FROM note WHERE id = :id LIMIT 1")
     suspend fun getNoteEntity(id: Long): NoteEntity?
